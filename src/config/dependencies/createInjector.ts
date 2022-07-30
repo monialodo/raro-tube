29b4b95b0a2919df1e010001757256631876ee9c
danielsantana77 <daniel_santana_oliveira@hotmail.com>
2022-07-29 23:25:35 -0300

    add Admin Crud

--- src/config/dependencies/createInjector.ts
+++ src/config/dependencies/createInjector.ts
@@ -1,17 +1,19 @@
 import { getCustomRepository } from "typeorm";
 import Container from "typedi";
 import { StudentRepository } from "../../repositories/studentsRepository";
-
+import { AdministratorRepository } from "../../repositories/AdministratorRepository";
 // inicializador de dependências:
 // inicializa controllers
 import "../../controllers/StudentController"
-
+import "../../controllers/AdministratorController"
 
 // inicializa services
 import "../../services/StudentService.ts";
+import "../../services/AdministratorService.ts"
 
 const createDependencyInjector = () => {
-  Container.set("StudentRepository", getCustomRepository(StudentRepository));
+  Container.set("StudentRepository", getCustomRepository(StudentRepository))
+  Container.set("AdministratorRepository", getCustomRepository(AdministratorRepository))
 };
 
 export default createDependencyInjector;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import * as express from "express";
 import createStudentRouter from "./studentRouter";
-
+import createAdministratorRouter from "./AdministratorRouter";
 const createRouters = (app: express.Express) => {
   app.use("/v1/students", createStudentRouter());
+  app.use('/v1/administrators', createAdministratorRouter())
 };
 
 export default createRouters;
