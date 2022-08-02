1d044810b924a4d4855843645fc099fafe664a04
Monia Lodo <monialodo@hotmail.com>
2022-08-02 19:34:37 -0300

    Ajuste para merge em develop

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,9 @@
 import * as express from "express";
 
-import createAdministratorRouter from "./administratorRouter";
-import createStudentRouter from "./studentRouter";
-import createSuperUserRouter from "./superUserRouter";
-import createTeacherRouter from "./teacherRouter";
-
 const createRouters = (app: express.Express) => {
-  app.use("/v1/students", createStudentRouter());
-  app.use("/v1/super-users", createSuperUserRouter());
-  app.use("/v1/teachers", createTeacherRouter());
-  app.use("/v1/administrators", createAdministratorRouter());
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
 };
 
 export default createRouters;
