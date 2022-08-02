5acb5bfdbcb90ff2fc4b20ae44e3c3b58a0f7a3d
Monia Lodo <monialodo@hotmail.com>
2022-08-02 19:33:26 -0300

    Ajuste para merge em develop

--- src/repositories/administratorRepository.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-
-import { IAdministratorRepository } from "../@types/repositories/IAdministratorRepository";
-import { Administrator } from "../models/administratorEntity";
-
-@EntityRepository(Administrator)
-export class AdministratorRepository
-  extends Repository<Administrator>
-  implements IAdministratorRepository {}
