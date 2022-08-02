a50af8e8c44a41f5336e33ab2aaaad9a6c1e4db1
danielsantana77 <daniel_santana_oliveira@hotmail.com>
2022-08-02 20:19:01 -0300

    update crud comments

--- src/@types/repositories/ICommentRepository.ts
+++ src/@types/repositories/ICommentRepository.ts
@@ -1,13 +1,6 @@
-
-import { DeleteResult, UpdateResult } from "typeorm";
+import { Repository } from "typeorm";
 import { Comment } from "../../models/commentEntity";
-import { CommentDTO } from "../dto/CommentDto";
 
 
-export interface ICommentRepository {
-  save(Comment: CommentDTO): Promise<Comment>;
-  find(): Promise<Comment[]>;
-  findOne(id: string): Promise<Comment>;
-  update(id: string, comment: Comment): Promise<UpdateResult>;
-  delete(id: string): Promise<DeleteResult>;
-}
+
+export type ICommentRepository = Repository<Comment>
