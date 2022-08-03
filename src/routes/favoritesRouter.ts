98e796490604c39bcb9399d106c6046e1dfaefb7
danielsantana77 <daniel_santana_oliveira@hotmail.com>
2022-08-02 21:15:50 -0300

    crud-favorites add routes

--- src/routes/favoritesRouter.ts
+++ src/routes/favoritesRouter.ts
@@ -10,9 +10,12 @@ const getController = (): FavoritesController => {
 };
 
 const createFavoritesRouter = () => {
-    router.get("/", (req, res) => getController().findAll(req, res));
+    router.get("", (req, res) => getController().findAll(req, res));
+    router.post("", (req, res) => getController().create(req, res));
     router.get("/:id", (req, res) => getController().find(req, res));
-    router.post("/", (req, res) => getController().create(req, res));
+    router.put("/:id", (req, res) => getController().update(req, res));
+    router.delete("/:id", (req, res) => getController().delete(req, res));
+
     return router;
 };
 
